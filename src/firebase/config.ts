7ca5dd215c0643d08aab5afe12fd24fe75d956c0
Andi Rifqi Aunur Rahman <andikah3954g@gmail.com>
2025-11-29 07:38:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4798208575-47d7c",
+  "appId": "1:789319513972:web:755b9835ae949456b31e21",
+  "apiKey": "AIzaSyA9zU2DEZtRhujBXe-_Ls2EX3NCjA-jw98",
+  "authDomain": "studio-4798208575-47d7c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "789319513972"
+};
